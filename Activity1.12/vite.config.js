661b02cb3c42dd408486bb50182d52d0316fe2d7
Ronel <ronelmelendrez01@gmail.com>
2024-12-09 01:30:27 +0800

    Initial commit

---
+++ Activity1.12/vite.config.js
@@ -0,0 +1,20 @@
+const isCodeSandbox =
+  "SANDBOX_URL" in process.env || "CODESANDBOX_HOST" in process.env;
+
+export default {
+  root: "src/",
+  publicDir: "../static/",
+  base: "./",
+  server: {
+    host: true,
+    open: !isCodeSandbox, // Open if it's not a CodeSandbox
+  },
+  build: {
+    outDir: "../dist",
+    emptyOutDir: true,
+    sourcemap: true,
+  },
+  optimizeDeps: {
+    include: ["three", "lil-gui"],
+  },
+};
